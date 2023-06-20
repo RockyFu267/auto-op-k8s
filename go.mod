0917a64cfc6b0fc6c4d33cc5f7919e145225db8a
RockyFu267 <453618045@qq.com>
2023-06-20 17:14:58 +0800

    update

--- go.mod
+++ go.mod
@@ -2,10 +2,11 @@ module autp-op-k8s
 
 go 1.20
 
+require k8s.io/api v0.27.1
+
 require (
 	github.com/google/uuid v1.3.0 // indirect
 	golang.org/x/tools v0.8.0 // indirect
-	k8s.io/api v0.27.1 // indirect
 )
 
 require (
@@ -39,7 +40,7 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/apimachinery v0.27.1 // indirect
+	k8s.io/apimachinery v0.27.1
 	k8s.io/client-go v0.27.1
 	k8s.io/klog/v2 v2.90.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230308215209-15aac26d736a // indirect
--- pkgfunc/controller.go
+++ pkgfunc/controller.go
@@ -1,37 +1,50 @@
 package pkgfunc
 
 import (
-	"log"
+	"context"
 	"reflect"
+	"time"
 
+	ACV1 "k8s.io/api/core/v1"
 	AN1 "k8s.io/api/networking/v1"
-	APPM1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	ICv1 "k8s.io/client-go/informers/core/v1"
-	INv1 "k8s.io/client-go/informers/networking/v1"
+	KAPAE "k8s.io/apimachinery/pkg/api/errors"
+	APAM1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	KAPPUR "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	CIC1 "k8s.io/client-go/informers/core/v1"
+	CIN1 "k8s.io/client-go/informers/networking/v1"
 	"k8s.io/client-go/kubernetes"
-	LCv12 "k8s.io/client-go/listers/core/v1"
-	LNv1 "k8s.io/client-go/listers/networking/v1"
+	CLC1 "k8s.io/client-go/listers/core/v1"
+	CLN1 "k8s.io/client-go/listers/networking/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
+)
+
+const (
+	workNum  = 5
+	maxRetry = 10
 )
 
 // controllerDemo demo
 type controllerDemo struct {
 	client        kubernetes.Interface
-	ingressLister LNv1.IngressLister
-	ServiceLister LCv12.ServiceLister
+	ingressLister CLN1.IngressLister
+	serviceLister CLC1.ServiceLister
 	queue         workqueue.RateLimitingInterface
 }
 
 func (c *controllerDemo) Run(stopCH chan struct{}) {
+	for i := 0; i < workNum; i++ {
+		go wait.Until(c.worker, time.Minute, stopCH)
+	}
 	<-stopCH
 }
 
-func NewController(client kubernetes.Interface, svcInformer ICv1.ServiceInformer, ingressInformer INv1.IngressInformer) controllerDemo {
+func NewController(client kubernetes.Interface, svcInformer CIC1.ServiceInformer, ingressInformer CIN1.IngressInformer) controllerDemo {
 	c := controllerDemo{
 		client:        client,
 		ingressLister: ingressInformer.Lister(),
-		ServiceLister: svcInformer.Lister(),
+		serviceLister: svcInformer.Lister(),
 		queue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "ingressManager"),
 	}
 	svcInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
@@ -59,7 +72,7 @@ func (c *controllerDemo) updateSvc(oldObj interface{}, newObj interface{}) {
 
 func (c *controllerDemo) deleteIngress(obj interface{}) {
 	ingressTMP := obj.(*AN1.Ingress)
-	ownerReference := APPM1.GetControllerOf(ingressTMP)
+	ownerReference := APAM1.GetControllerOf(ingressTMP)
 	if ownerReference == nil {
 		return
 	}
@@ -73,9 +86,121 @@ func (c *controllerDemo) deleteIngress(obj interface{}) {
 func (c *controllerDemo) enqueue(obj interface{}) {
 	key, err := cache.MetaNamespaceKeyFunc(obj)
 	if err != nil {
-		log.Panicln(err)
+		KAPPUR.HandleError(err)
 		return
 	}
 
 	c.queue.Add(key)
+}
+
+func (c *controllerDemo) worker() {
+	for c.processNextItem() {
+
+	}
+}
+
+func (c *controllerDemo) processNextItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	defer c.queue.Done(item)
+
+	key := item.(string)
+
+	err := c.syncService(key)
+	if err != nil {
+		c.handlerError(key, err)
+	}
+	return true
+}
+func (c *controllerDemo) syncService(key string) error {
+	namespaceKey, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return err
+	}
+
+	//删除
+	service, err := c.serviceLister.Services(namespaceKey).Get(name)
+	if KAPAE.IsNotFound(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	//新增和删除
+	_, ok := service.GetAnnotations()["ingress/http"]
+	ingress, err := c.ingressLister.Ingresses(namespaceKey).Get(name)
+	if err != nil && !KAPAE.IsNotFound(err) {
+		return err
+	}
+
+	if ok && KAPAE.IsNotFound(err) {
+		//create ingress
+		ig := c.constructIngress(service)
+		_, err := c.client.NetworkingV1().Ingresses(namespaceKey).Create(context.TODO(), ig, APAM1.CreateOptions{})
+		if err != nil {
+			return err
+		}
+	} else if !ok && ingress != nil {
+		//delete ingress
+		err := c.client.NetworkingV1().Ingresses(namespaceKey).Delete(context.TODO(), name, APAM1.DeleteOptions{})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *controllerDemo) handlerError(key string, err error) {
+	if c.queue.NumRequeues(key) <= maxRetry {
+		c.queue.AddRateLimited(key)
+		return
+	}
+
+	KAPPUR.HandleError(err)
+	c.queue.Forget(key)
+}
+
+func (c *controllerDemo) constructIngress(service *ACV1.Service) *AN1.Ingress {
+	ingress := AN1.Ingress{}
+
+	ingress.ObjectMeta.OwnerReferences = []APAM1.OwnerReference{
+		*APAM1.NewControllerRef(service, ACV1.SchemeGroupVersion.WithKind("Service")),
+	}
+
+	ingress.Name = service.Name
+	ingress.Namespace = service.Namespace
+	pathType := AN1.PathTypePrefix
+	icn := "nginx"
+	ingress.Spec = AN1.IngressSpec{
+		IngressClassName: &icn,
+		Rules: []AN1.IngressRule{
+			{
+				Host: "example.com",
+				IngressRuleValue: AN1.IngressRuleValue{
+					HTTP: &AN1.HTTPIngressRuleValue{
+						Paths: []AN1.HTTPIngressPath{
+							{
+								Path:     "/",
+								PathType: &pathType,
+								Backend: AN1.IngressBackend{
+									Service: &AN1.IngressServiceBackend{
+										Name: service.Name,
+										Port: AN1.ServiceBackendPort{
+											Number: 80,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return &ingress
 }
